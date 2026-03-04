df4b1c45c47b9b5925a2f38b4d8968aa83e93a06
shankar117512 <shankar117512@gmail.com>
2026-03-04 16:53:58 +0000

    views added

--- members/views.py
+++ members/views.py
@@ -1,10 +1,5 @@
 from django.http import HttpResponse
-from django.template import loader
 
-def testing(request):
-  template = loader.get_template('template.html')
-  context = {
-    'firstname': 'Linus',
-  }
-  return HttpResponse(template.render(context, request))
+def main(request):
+    return HttpResponse("Hello from Members App")
 
