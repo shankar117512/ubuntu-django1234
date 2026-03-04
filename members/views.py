a6e5140e78319902968ed543c829649813237118
shankar117512 <shankar117512@gmail.com>
2026-03-04 15:21:50 +0000

    views && urls

--- members/views.py
+++ members/views.py
@@ -22,3 +22,10 @@ def main(request):
   template = loader.get_template('main.html')
   return HttpResponse(template.render())
 
+def testing(request):
+  template = loader.get_template('template.html')
+  context = {
+    'fruits': ['Apple', 'Banana', 'Cherry'],   
+  }
+  return HttpResponse(template.render(context, request))
+
